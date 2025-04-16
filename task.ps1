c17fdebf7d9b11c6848a018819a29d209400266a
Pavlo Nikolaiev <workhardpolly@gmail.com>
2025-04-16 10:18:18 +0200

    solution

--- task.ps1
+++ task.ps1
@@ -49,6 +49,6 @@ $Params = @{
     Publisher          = 'Microsoft.Azure.Extensions'
     ExtensionType      = 'CustomScript'
     TypeHandlerVersion = '2.1'
-    Settings          = @{fileUris = @('https://raw.githubusercontent.com/panik10/azure_task_12_deploy_app_with_vm_extention/refs/heads/develop/install-app.sh'); commandToExecute = './install-app.sh'}
+    Settings          = @{fileUris = @('https://raw.githubusercontent.com/panik10/azure_task_12_deploy_app_with_vm_extention/refs/heads/main/install-app.sh'); commandToExecute = './install-app.sh'}
 }
 Set-AzVMExtension @Params
