84aa83d4e3d7a203b1d4d59ce49422f95287621a
Pavlo Nikolaiev <workhardpolly@gmail.com>
2025-04-16 10:04:20 +0200

    solution

--- task.ps1
+++ task.ps1
@@ -40,3 +40,15 @@ New-AzVm `
 -SshKeyName $sshKeyName  -PublicIpAddressName $publicIpAddressName
 
 # ↓↓↓ Write your code here ↓↓↓
+
+
+$Params = @{
+    ResourceGroupName  = $resourceGroupName
+    VMName             = $vmName
+    Name               = 'DeployTodoAppScript'
+    Publisher          = 'Microsoft.Azure.Extensions'
+    ExtensionType      = 'CustomScript'
+    TypeHandlerVersion = '2.1'
+    Settings          = @{fileUris = @('https://raw.githubusercontent.com/panik10/azure_task_12_deploy_app_with_vm_extention/refs/heads/develop/install-app.sh'); commandToExecute = './install-app.sh'}
+}
+Set-AzVMExtension @Params
